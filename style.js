067eaafe786cc0c27749074af5ff6d09d2b384af
monu11296 <monu11296@gmail.com>
2018-10-03 01:51:34 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,174 @@
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+		
+	.btn2 {
+	        font-size: 0.8em;
+  position: relative;
+
+  display: block;
+  margin: 5px auto;
+  padding: 0;
+
+  overflow: hidden;
+
+  border-width: 0;
+  outline: none;
+  border-radius: 2px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, .6);
+  
+  background-color: #2ecc71;
+  color: #ecf0f1;
+  
+  transition: background-color .3s;
+}
+
+.btn2:hover, .btn:focus {
+  background-color: #27ae60;
+}
+
+.btn2 > * {
+  position: relative;
+}
+
+.btn2 span {
+  display: block;
+  padding: 12px 24px;
+}
+
+.btn2:before {
+  content: "";
+  
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  
+  display: block;
+  width: 0;
+  padding-top: 0;
+    
+  border-radius: 100%;
+  
+  background-color: rgba(236, 240, 241, .3);
+  
+  -webkit-transform: translate(-50%, -50%);
+  -moz-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  -o-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+
+.btn2:active:before {
+  width: 120%;
+  padding-top: 120%;
+  
+  transition: width .2s ease-out, padding-top .2s ease-out;
+}
+
+
+*, *:before, *:after {
+  box-sizing: border-box;
+}
+
+
+.btn2.orange {
+  background-color: #e67e22;
+}
+
+.btn2.orange:hover, .btn.orange:focus {
+  background-color: #d35400;
+}
+
+.btn2.red {
+  background-color: #e74c3c;
+}
+
+.btn2.red:hover, .btn.red:focus {
+  background-color: #c0392b;
+}
+	
+	
+		header {
+			margin-bottom: 20px;
+		}
+		.outline {
+			width: 302px;
+			height: 252px;
+			border: 1px solid #cccccc;
+			border-radius: 4px;
+			background-color: #f2dede;
+		}
+		.pub_300x250 {
+			width: 300px;
+			height: 250px;
+			background-color: #dff0d8;
+		}
+		h5.bg-success,
+		h5.bg-danger {
+			padding: 8px;
+			border: 1px solid #cccccc;
+			border-radius: 4px;
+		}
+		pre {
+			background-color: #f0f0f0;
+		}
+		
+		#adblockdetected{
+		display:none;
+		}
+
+			#mainbox{
+					position: fixed;
+	                top: 15%;
+	                width: 60%;
+	                min-height: 18px;
+	                background-color: rgb(255, 255, 255);
+	                /*z-index: 10000; */
+	                font-family: Segoe UI ;
+	                line-height: normal !important;
+	                font-size: 16pt !important;.
+	                text-align: center !important;
+	                padding: 12px !important;
+	                display: block;
+	                margin-left: 20%;
+	                /*margin-right: 2px;*/
+	                border-radius: 15px;
+	                box-shadow: rgba(0, 0, 0, 0.298039) 0px 14px 24px -8px;
+	                visibility: visible;
+	                text-align: center;
+                }
+                
+        #mainbox h3{
+                color:#999;font-size:20pt;color:#777777;font-family:Segoe UI, sans-serif;font-weight:200;margin-top:10px;margin-bottom:10px;text-align:center;
+		}
+		
+		#mainbox h1{
+                font-size:22pt;font-weight:500;font-family:Segoe UI, sans-serif;color:#777777;margin-top:10px;margin-bottom:10px;text-align:center;
+		}
+		
+		#mainbox hr{
+                 display: block;margin-top: 0.5em;margin-bottom: 0.5em;margin-left: auto;margin-right: auto; border:1px solid #CCC; width: 25%;text-align:center;
+		}
+		
+		#mainbox p{
+                 font-family:Segoe UI, sans-serif;font-weight:300;font-size:9pt;color:#777777;text-align:center;
+		}
+
+		#maincontent {
+			text-align: center;
+			font-family: 'Raleway', sans-serif;
+		}
+
+#AdS1{		width: 300px;
+            height: 250px;
+            border-style:dotted;
+            margin-left: auto;
+            margin-right: auto;
+    
+}
